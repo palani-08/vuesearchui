b8200569b85894665c6762c12e3d547a28d4fe02
palani-08 <palani7pavan@gmail.com>
2021-06-01 20:03:01 +0530

    serach ui

--- ais-ecommerce-demo-app/src/main.js
+++ ais-ecommerce-demo-app/src/main.js
@@ -1,12 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
 import InstantSearch from 'vue-instantsearch';
-createApp(App).use(store).use(router).mount("#app")Vue.use(InstantSearch);
+
+Vue.use(InstantSearch);
 
 new Vue({
   el: '#app',
   render: h => h(App),
 });
-
